bbf3de8f610dfaaf3b28305a61e10171eda86a00
SouhardaBanik <souhardabanik@gmail.com>
2026-02-16 11:12:32 +0530

    UC1 Completed

---
+++ OOPSBannerApp.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class OOPSBannerApp
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("OOPS");
+    }
+}
